8a5b6d77c6d8d3e41f526da6fc7e10b65de8980d
nutsaaa <nutsa.vashakidze.1@btu.edu.ge>
2020-12-16 05:07:10 +0400

    QULA MINDA

---
+++ app/src/main/java/com/example/arviciradavarqva/PersonActivity.kt
@@ -0,0 +1,41 @@
+package com.example.arviciradavarqva
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+class PersonActivity : AppCompatActivity() {
+
+    private lateinit var userInfoTextView: TextView
+    private lateinit var passwordChangeButton: Button
+    private lateinit var logoutButton: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_person)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        userInfoTextView = findViewById(R.id.UserInfoTextView)
+        passwordChangeButton = findViewById(R.id.gotoChangePasswordButton)
+        logoutButton = findViewById(R.id.logoutButton)
+
+        userInfoTextView.text = mAuth.currentUser?.uid
+
+        passwordChangeButton.setOnClickListener {
+             startActivity(Intent(this, PasswordChangeActivity::class.java))
+        }
+
+        logoutButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+            mAuth.signOut()
+        }
+    }
+}
